a6d636bd55d98cc15d05709f61569bb5eb5ffdd8
akatomakhina <katomakhinaanastasiya@gmail.com>
2019-01-28 23:10:11 +0200

    sto: update Controller

--- src/main/java/by/katomakhin/app/sto/controller/CertificateOfCompletionController.java
+++ src/main/java/by/katomakhin/app/sto/controller/CertificateOfCompletionController.java
@@ -1,12 +1,17 @@
 package by.katomakhin.app.sto.controller;
 
 import by.katomakhin.app.sto.conf.ApplicationConfig;
+import by.katomakhin.app.sto.model.certificate.CertificateOfCompletion;
 import by.katomakhin.app.sto.service.dev.CertificateOfCompletionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 //@ContextConfiguration(classes = ApplicationConfig.class)
@@ -19,9 +24,12 @@ public class CertificateOfCompletionController {
         this.service = service;
     }
 
-    @GetMapping(value = "/cert/{licensePlate}")
-    public String getCertificate (String licensePlate){
-
+    @GetMapping(value = "/cert/{model}/{licensePlate}")
+    public ModelAndView getCertificate (ModelAndView modelAndView,@PathVariable String model, @PathVariable String licensePlate){
+        List<CertificateOfCompletion> cert = service.getCompleteCertificateOfCompletion(model, licensePlate);
+        modelAndView.addObject(cert);
+        modelAndView.setViewName("workList");
+        return modelAndView;
     }
 }
                    
